1d1c6fb4026b7b8f4a65f456357a2ebe5511bae7
ScOreX-242 <israfilovamin@mail.ru>
2026-01-09 02:16:37 +0400

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * print_char - prints char
- * @args: arguments
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned.
  */
-int print_char(va_list *args)
+int _putchar(char c)
 {
-	char c = va_arg(*args, int);
-
-	write(1, &c, 1);
-	return (1);
+	return (write(1, &c, 1));
 }
