48664180c326f33c98d88eb521fcbaaaa7625dd0
ScOreX-242 <israfilovamin@mail.ru>
2026-01-09 02:03:43 +0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,12 +1,10 @@
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _printf - produces output according to a format.
  * @format: character string containing directives.
  *
- * Return: the number of characters printed (excluding the null byte).
+ * Return: the number of characters printed.
  */
 int _printf(const char *format, ...)
 {
@@ -14,37 +12,35 @@ int _printf(const char *format, ...)
 	int i = 0, count = 0;
 	char *str;
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 
 	va_start(args, format);
-	while (format[i])
+	while (format && format[i])
 	{
 		if (format[i] != '%')
 		{
-			count += putchar(format[i]);
+			count += _putchar(format[i]);
 		}
 		else
 		{
 			i++;
-			if (format[i] == '\0')
-				return (-1);
 			if (format[i] == 'c')
-				count += putchar(va_arg(args, int));
+				count += _putchar(va_arg(args, int));
 			else if (format[i] == 's')
 			{
 				str = va_arg(args, char *);
-				if (str == NULL)
+				if (!str)
 					str = "(null)";
 				while (*str)
-					count += putchar(*str++);
+					count += _putchar(*str++);
 			}
 			else if (format[i] == '%')
-				count += putchar('%');
+				count += _putchar('%');
 			else
 			{
-				count += putchar('%');
-				count += putchar(format[i]);
+				count += _putchar('%');
+				count += _putchar(format[i]);
 			}
 		}
 		i++;
